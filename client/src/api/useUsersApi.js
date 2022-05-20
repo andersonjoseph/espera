363e6515be258988b34fe273183b6e5f8d288577
DESKTOP-9PLJV5Q\Anderson <andersonjoseph@mailfence.com>
2022-05-20 18:04:54 -0400

    add api handler for users

---
+++ client/src/api/useUsersApi.js
@@ -0,0 +1,20 @@
+import { ApiClient } from './ApiClient';
+
+export function useUsersApi() {
+  return {
+    getUsers: async () => (await ApiClient.instance.get('users')).data,
+    getUsersByWaitlist: async (waitlist) =>
+      (
+        await ApiClient.instance.get('users', {
+          params: { waitlist },
+        })
+      ).data,
+    getUser: async (id) => (await ApiClient.instance.get(`users/${id}`)).data,
+    createUser: async (data) =>
+      (await ApiClient.instance.post('users', data)).data,
+    updateUser: async (id, data) =>
+      (await ApiClient.instance.patch(`users/${id}`, data)).data,
+    deleteUser: async (id) =>
+      (await ApiClient.instance.delete(`users/${id}`)).data,
+  };
+}
